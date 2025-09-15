e4c088976de41febb91f1a5f0f72f06922afbd72
gurkanyigitgr <gurkanyigitgr@gmail.com>
2025-09-15 23:33:49 +0300

    Implement array with user input demo

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+
+        Scanner scan = new Scanner(System.in);
+
+        for(int i = 0; i< arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for(int i = 0; i< arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
