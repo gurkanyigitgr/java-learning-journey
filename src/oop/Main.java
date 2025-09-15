cd5abc8e66a914029793851c515a040214964a86
gurkanyigitgr <gurkanyigitgr@gmail.com>
2025-09-15 14:20:19 +0300

    Add basic OOP calculator example

---
+++ src/oop/Main.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Main {
+    public static void main(String[] args) {
+        calculator calc1 = new calculator();
+        calc1.num1 = 20;
+        calc1.num2 = 30;
+        System.out.println(calc1.num1);
+        System.out.println(calc1.num2);
+    }
+}
---
+++ src/oop/calculator.java
@@ -0,0 +1,6 @@
+package oop;
+
+public class calculator {
+    public int num1;
+    public int num2;
+}
