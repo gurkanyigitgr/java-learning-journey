91bc512f5503efde873e0e1ae40bcb741c23e857
gurkanyigitgr <gurkanyigitgr@gmail.com>
2025-09-15 13:31:36 +0300

    Add Fibonacci function to recursive functions example

--- src/functions/functionRecursive.java
+++ src/functions/functionRecursive.java
@@ -7,8 +7,18 @@ public class functionRecursive {
         }
         return x + r(x-1);
     }
+
+    static int fibo(int n){
+        if(n==1 || n==2) {
+            return 1;
+        }
+
+        return fibo(n-1)+ fibo(n-2);
+    }
     public static void main(String[] args) {
         System.out.println(r(10));
+
+        System.out.println(fibo(5));
 
     }
 }
